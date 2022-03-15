3277a6cee797693eb2606160b1035ee249feeb09
Benatovich <sambenatovich@gmail.com>
2022-03-14 20:45:18 -0400

    first test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,16 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Order = require('../lib/models/Order');
-
-// TODO: Remove this function & use the Order model
-async function createOrder({ product, quantity }) {
-  const { rows } = await pool.query(
-    'INSERT INTO orders(product, quantity) VALUES ($1, $2) RETURNING *;',
-    [product, quantity]
-  );
-  return new Order(rows[0]);
-}
-
 // TODO: Remove this function & use the Order model
 async function getOrderById(id) {
   const { rows } = await pool.query(
@@ -35,14 +25,15 @@ describe('refactory routes', () => {
   });
 
   it('should be able to create an order', async () => {
-    const res = await request(app)
-      .post('/api/v1/orders')
-      .send({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'bagels', quantity: 12 });
+    const res = await request(app).get(`/api/v1/orders/${order.id}`);
 
+    expect(res.body).toEqual(order);
+  
     expect(res.body).toEqual({
       id: expect.any(String),
-      product: 'Widget',
-      quantity: 1,
+      product: 'bagels',
+      quantity: 12,
     });
   });
 
